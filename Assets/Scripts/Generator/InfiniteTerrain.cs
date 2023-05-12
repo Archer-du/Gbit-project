20ab9f2680f0fffcab3a4a905f8d10209159faa5
GitHub cqf2186863072 <GitHub 2186863072@qq.com>
2023-05-12 19:51:06 +0800

    Terrain Generator completed

---
+++ Assets/Scripts/Generator/InfiniteTerrain.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteTerrain : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private GameObject[] prefabList;
+    [SerializeField] private int maxTerrains = 3;
+    [SerializeField] private float spawn_X_Tr = 0;
+    [SerializeField] private float terrainLength = 20f;
+
+    private Queue<GameObject> activeTerrains;
+    private Queue<int> usedTerrainIndices;
+
+    private void Start()
+    {
+        usedTerrainIndices = new Queue<int>();
+        activeTerrains = new Queue<GameObject>();
+        for (int i = 0; i < maxTerrains; i++)
+        {
+            SpawnTerrain();
+        }
+    }
+
+    private void Update()
+    {
+        if (playerTransform.position.x > (activeTerrains.Peek().transform.position.x + terrainLength))
+        {
+            DeleteTerrain();
+            SpawnTerrain();
+        }
+    }
+
+    private void SpawnTerrain()
+    {
+        int prefabIndex = Random.Range(0, prefabList.Length);
+        while (usedTerrainIndices.Contains(prefabIndex))
+        {
+            prefabIndex = Random.Range(0, prefabList.Length);
+        }
+        GameObject prefab = Instantiate(prefabList[prefabIndex]);
+        prefab.transform.SetParent(transform);
+        prefab.transform.position = Vector3.right * spawn_X_Tr;
+        spawn_X_Tr += terrainLength;
+        usedTerrainIndices.Enqueue(prefabIndex);
+        while (usedTerrainIndices.Count > 2)
+        {
+            usedTerrainIndices.Dequeue();
+        }
+        activeTerrains.Enqueue(prefab);
+    }
+
+    private void DeleteTerrain()
+    {
+        usedTerrainIndices.Dequeue();
+        Destroy(activeTerrains.Dequeue());
+    }
+}
